bb874a2621cb839e153d0e63ad18698c6481de86
spookypeanut <github@spookypeanut.co.uk>
2020-10-24 23:30:02 +0100

    Mostly there

--- python/justplayit/launcher.py
+++ python/justplayit/launcher.py
@@ -1,6 +1,9 @@
+import os
 import logging
 from mpd import MPDClient
 from mpd.base import ConnectionError
+
+BOOKDIR = "/var/books"
 
 class MPDConnection(object):
     def __init__(self):
@@ -28,6 +31,12 @@ class MPDConnection(object):
         self._ensure_connected()
         return self.client.playlist()
 
+    def add_to_playlist(self, to_add):
+        if isinstance(to_add, str):
+            to_add = [to_add]
+        for eachone in to_add:
+            self.client.add(eachone)
+
 
 MPD = MPDConnection()
 
@@ -35,25 +44,36 @@ def main():
     log_format = '[%(asctime)s: %(levelname)s] %(message)s'
     logging.basicConfig(format=log_format, level=logging.DEBUG)
     ensure_playing()
-    new_tracks = get_new_tracks()
-    add_tracks_to_playlist()
+    new_books = get_new_books()
+    logging.info("New books: %s", new_books)
+    MPD.add_to_playlist(new_books)
 
 
 def ensure_playing():
     logging.info("ensure_playing")
 
 
-def get_new_tracks():
+def get_all_books():
+    return sorted(os.listdir(BOOKDIR))
+
+
+def get_new_books():
     old_tracks = set(MPD.get_tracks_on_playlist())
     logging.info("%s tracks on playlist" % (len(old_tracks)))
     logging.debug("First 10: %s", list(old_tracks)[:10])
-    raise Exception
-    all_tracks = get_all_track_files()
-    new_tracks = []
-    for each_track in all_tracks:
-        if each_track not in old_tracks:
-            new_tracks.append(each_track)
-    return new_tracks
+    all_books = get_all_books()
+    logging.info("Books: %s", all_books)
+    new_books = []
+    for each_book in all_books:
+        found_book = False
+        for old_track in old_tracks:
+            if each_book in old_track:
+                found_book = True
+                break
+        if found_book is True:
+            continue
+        new_books.append(each_book)
+    return new_books
 
 
 if __name__ == "__main__":
